02b2866fbabdec6d56d1bde21590ce4d89c6b5b8
Manish Pakhira <48309293+manishmcu@users.noreply.github.com>
2020-04-25 22:43:50 +0530

    Add files via upload

---
+++ ADXL335_XYZbot/ADXL335_XYZbot.ino
@@ -0,0 +1,79 @@
+#include <ADXL335.h>
+
+int xA;
+int yA;
+int zA;
+
+int LM1 = 11;       // left motor
+int LM2 = 10;       // left motor
+int RM1 = 9;       // right motor
+int RM2 = 5;      // right motor
+ 
+void setup() {
+  Serial.begin(9600);
+  pinMode(LM1, OUTPUT);
+  pinMode(LM2, OUTPUT);
+  pinMode(RM1, OUTPUT);
+  pinMode(RM2, OUTPUT);
+
+}
+ 
+void loop() {
+ 
+  xA = analogRead(A0);
+  yA = analogRead(A1);
+  zA = analogRead(A2);
+ 
+ if(zA > 395)     // Move Forward
+
+  {
+    analogWrite(LM1, 100);
+    analogWrite(LM2, LOW);
+    analogWrite(RM1, 100);
+    analogWrite(RM2, LOW);
+  }
+  
+  if(xA > 395)     // Turn right
+  {
+    analogWrite(LM1, LOW);
+    analogWrite(LM2, LOW);
+    analogWrite(RM1, 100);
+    analogWrite(RM2, LOW);
+    delay(25);
+  }
+  
+  if(xA < 270)     // turn left
+  {
+    analogWrite(LM1, 100);
+    analogWrite(LM2, LOW);
+    analogWrite(RM1, LOW);
+    analogWrite(RM2, LOW);
+    delay(25);
+  } 
+  if(zA < 270)     // stop
+  {
+    analogWrite(LM1, LOW);
+    analogWrite(LM2, LOW);
+    analogWrite(RM1, LOW);
+    analogWrite(RM2, LOW);
+  }
+  if(zA < 270)     // back
+  {
+    analogWrite(LM1, LOW);
+    analogWrite(LM2, 80);
+    analogWrite(RM1, LOW);
+    analogWrite(RM2, 80);
+  }
+  
+
+  if (xA > 395) {
+    Serial.println(" X - - ist oben");
+  } 
+  else if (yA > 395) {
+    Serial.println(" - Y - ist oben");
+  }
+  else if (zA > 395) {
+    Serial.println(" - - Z ist oben");
+  }
+ 
+}
